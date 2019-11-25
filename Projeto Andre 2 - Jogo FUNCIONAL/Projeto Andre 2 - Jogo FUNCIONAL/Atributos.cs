fe064892a1b905491131c3b3ba7b737498756baf
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 14:16:53 -0300

    Adição da Classe de Atributos
    Classe necessária para Classe "Jogador".

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Atributos.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public class Atributos
+	{
+		public int Vida{get;set;}
+		public int Mana{get;set;}
+		public int Forca{get;set;}
+		public int Agilidade{get;set;}
+		public int Inteligencia{get;set;}
+		public int Armadura{get;set;}
+		
+		public Atributos(string r)
+		{
+			//Adicionar switch para distribuição de pontos por Raça
+		}
+	}
+}
