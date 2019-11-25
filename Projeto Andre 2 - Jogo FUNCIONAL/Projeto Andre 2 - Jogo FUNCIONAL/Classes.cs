ae2c1c2881107ccb462c8b0490d1cd094a054495
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 15:40:37 -0300

    Adição Classe "Classes"
    Classe necessária para "Jogador".

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public abstract class Classes
+	{
+		public string NomeMg1{get;set;}
+		public string NomeMg2{get;set;}
+		public string NomeMg3{get;set;}
+		public string ManaMg1{get;set;}
+		public string ManaMg2{get;set;}
+		public string ManaMg3{get;set;}
+		
+		public abstract void Magia1();
+		public abstract void Magia2();
+		public abstract void Magia3();
+	}
+}
