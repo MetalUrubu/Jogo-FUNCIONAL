588703d5c35db125be979e2617cbd942763ea4d3
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 16:27:40 -0300

    Atualização em "Atributos"
    Adicionado atributos Level, Pontos.
    Adicionados Seguintes Métodos:
    Evoluir
    addAtributo
    subAtributo

--- Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Atributos.cs
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Atributos.cs
@@ -11,10 +11,84 @@ namespace Projeto_Andre_2___Jogo_FUNCIONAL
 		public int Agilidade{get;set;}
 		public int Inteligencia{get;set;}
 		public int Armadura{get;set;}
+		public int Level{get;set;}
+		public int Pontos{get;set;}
 		
 		public Atributos(string r)
 		{
-			//Adicionar switch para distribuição de pontos por Raça
+			Pontos += 10;
+			Evoluir();
+			switch(r)
+			{
+				case "Humano":
+					addAtributo("Força",2);
+					addAtributo("Agilidade",5);
+					addAtributo("Inteligencia",3);
+					break;
+				case "Troll":
+					addAtributo("Força",5);
+					addAtributo("Agilidade",2);
+					addAtributo("Inteligencia",3);
+					break;
+				case "Elfo":
+					addAtributo("Força",2);
+					addAtributo("Agilidade",2);
+					addAtributo("Inteligencia",6);
+					break;
+			}
+		}
+		
+		public void addAtributo(string atbt, int qntd)
+		{
+			if(Pontos-=qntd >=0)
+			{
+				Pontos -= qntd;
+				switch(atbt)
+				{
+					case "Força":
+						Forca += qntd;
+						break;
+					case "Agilidade":
+						Agilidade += qntd;
+						break;
+					case "Inteligencia":
+						Inteligencia += qntd;
+						break;
+				}
+			}
+		}
+		public void subAtributo(string atbt, int qntd)
+		{
+			switch(atbt)
+			{
+				case "Força":
+					if(Forca -= qntd > 0)
+					{
+						Forca += qntd;
+						Pontos += qntd;
+					}
+					break;
+				case "Agilidade":
+					if(Agilidade -= qntd > 0)
+					{
+						Agilidade += qntd;
+						Pontos += qntd;
+					}
+					break;
+				case "Inteligencia":
+					if(Inteligencia -= qntd > 0)
+					{
+						Inteligencia += qntd;
+						Pontos += qntd;
+					}
+					break;
+			}
+		}
+		public void Evoluir()
+		{
+			Level += 1;
+			Pontos += 5;
 		}
 	}
 }
+
