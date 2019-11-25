478b020cab2c06a71301c063ff511e02f213aa71
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 15:54:49 -0300

    Adição da Classe "Guerreiro", e update em "Classes

--- Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Classes.cs
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Classes.cs
@@ -7,9 +7,9 @@ namespace Projeto_Andre_2___Jogo_FUNCIONAL
 		public string NomeMg1{get;set;}
 		public string NomeMg2{get;set;}
 		public string NomeMg3{get;set;}
-		public string ManaMg1{get;set;}
-		public string ManaMg2{get;set;}
-		public string ManaMg3{get;set;}
+		public int ManaMg1{get;set;}
+		public int ManaMg2{get;set;}
+		public int ManaMg3{get;set;}
 		
 		public abstract void Magia1();
 		public abstract void Magia2();
---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Guerreiro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public class Guerreiro : Classes
+	{
+		public Guerreiro()
+		{
+			this.NomeMg1 = "Generico";
+			this.NomeMg2 = "Generico";
+			this.NomeMg3 = "Generico";
+			this.ManaMg1 = 1;
+			this.ManaMg2 = 2;
+			this.ManaMg3 = 3;
+		}
+	}
+}
