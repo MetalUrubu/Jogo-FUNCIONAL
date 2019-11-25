ab7411a7fdd583d9f2d6ffd3d1148f7a1c3495bc
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 14:12:13 -0300

    Adição inicial da classe Jogador

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Jogador.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public class Jogador
+	{
+		public string Nome{get;set;}
+		public string Raca{get;set;}
+		public Jogador()
+		{
+		}
+		public Jogador(string n, string r, string c)
+		{
+			Nome = n;
+			Raca = r;
+			//Adicionar switch para definição de classe
+		}
+	}
+}
