250f5a37831206e04344f4b92a3d24a0f36cf5e9
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-26 15:26:14 -0300

    Adição de form de informações sobre raças

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/RacasInfoForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	partial class RacasInfoForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(71, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(201, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "texto";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Humano:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// RacasInfoForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "RacasInfoForm";
+			this.Text = "RacasInfoForm";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/RacasInfoForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public partial class RacasInfoForm : Form
+	{
+		static FileStream racasInfoArquivo = new FileStream("RacasInfoText.txt",FileMode.Open);
+		static StreamReader sr = new StreamReader(racasInfoArquivo);
+		public RacasInfoForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
