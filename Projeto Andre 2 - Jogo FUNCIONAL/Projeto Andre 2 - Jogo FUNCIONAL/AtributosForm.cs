b6142856fb2d5d3c2e09788f8a1b178abaea7bbc
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-25 21:19:24 -0300

    Adição do Form de Atributos
    Usado pelo jogador para distribuir pontos de atributos

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/AtributosForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public partial class AtributosForm : Form
+	{
+		public AtributosForm()
+		{
+			InitializeComponent();
+		}
+		void ForcaAddClick(object sender, EventArgs e)
+		{
+	
+		}
+		void AgilidadeAddClick(object sender, EventArgs e)
+		{
+	
+		}
+		void InteligenciaAddClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ForcaSubClick(object sender, EventArgs e)
+		{
+	
+		}
+		void AgilidadeSubClick(object sender, EventArgs e)
+		{
+	
+		}
+		void InteligenciaSubClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ConfirmarClick(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
