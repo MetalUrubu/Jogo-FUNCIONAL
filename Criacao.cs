41bea7c38ffb160245b8ca4bdfcd57a7ab4d4f23
victorgomes1234 <50429042+victorgomes1234@users.noreply.github.com>
2019-11-25 21:52:09 -0300

    update no form

---
+++ Criacao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public partial class Criacao : Form
+	{
+		string nome, raca, classe;
+		public Criacao()
+		{
+			InitializeComponent();
+		}
+		void HumanoRadioCheckedChanged(object sender, EventArgs e)
+		{
+			raca = humanoRadio.Text;
+		}
+		void TrollRadioCheckedChanged(object sender, EventArgs e)
+		{	
+			raca = trollRadio.Text;
+		}
+		void ElfoRadioCheckedChanged(object sender, EventArgs e)
+		{
+			raca = elfoRadio.Text;
+		}
+		void GuerreiroRadioCheckedChanged(object sender, EventArgs e)
+		{
+			classe = guerreiroRadio.Text;
+		}
+		void MagoRadioCheckedChanged(object sender, EventArgs e)
+		{
+			classe = magoRadio.Text;
+		}
+		void ArqueiroRadioCheckedChanged(object sender, EventArgs e)
+		{
+			classe = arqueiroRadio.Text;
+		}
+		void NomeTextFieldTextChanged(object sender, EventArgs e)
+		{
+			nome = nomeTextField.Text;
+		}
+		void RacaInfoButtonClick(object sender, EventArgs e)
+		{
+			
+		}
+		void ClasseInforButtonClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ConfrimarButtonClick(object sender, EventArgs e)
+		{
+			Program.usuario = new Jogador(nome, raca, classe);
+			Program.atbtMenu = new AtributosForm();
+			Close();
+			Program.atbtMenu.Show();
+		}
+	}
+}
