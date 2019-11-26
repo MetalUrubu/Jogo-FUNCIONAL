03b1704fb75ab2c4361b7a5bac4a542c241c66c2
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-26 14:16:22 -0300

    Att "Atributos".

--- Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Atributos.cs
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Atributos.cs
@@ -14,81 +14,62 @@ namespace Projeto_Andre_2___Jogo_FUNCIONAL
 		public int Level{get;set;}
 		public int Pontos{get;set;}
 		
-		public Atributos(string r)
+		public Atributos()
 		{
-			Pontos += 10;
-			Evoluir();
-			switch(r)
-			{
-				case "Humano":
-					addAtributo("Força",2);
-					addAtributo("Agilidade",5);
-					addAtributo("Inteligencia",3);
-					break;
-				case "Troll":
-					addAtributo("Força",5);
-					addAtributo("Agilidade",2);
-					addAtributo("Inteligencia",3);
-					break;
-				case "Elfo":
-					addAtributo("Força",2);
-					addAtributo("Agilidade",2);
-					addAtributo("Inteligencia",6);
-					break;
-			}
+			Pontos += 20;
+			Level +=1;
+			
 		}
 		
 		public void addAtributo(string atbt, int qntd)
 		{
-			if((Pontos-=qntd) >=0)
-			{
-				Pontos -= qntd;
-				switch(atbt)
-				{
-					case "Força":
-						Forca += qntd;
-						break;
-					case "Agilidade":
-						Agilidade += qntd;
-						break;
-					case "Inteligencia":
-						Inteligencia += qntd;
-						break;
-				}
-			}
-		}
-		public void subAtributo(string atbt, int qntd)
-		{
+			Pontos -= qntd;
 			switch(atbt)
 			{
 				case "Força":
-					if((Forca -= qntd) > 0)
-					{
-						Forca += qntd;
-						Pontos += qntd;
-					}
+					Forca += qntd;
 					break;
 				case "Agilidade":
-					if((Agilidade -= qntd) > 0)
-					{
-						Agilidade += qntd;
-						Pontos += qntd;
-					}
+					Agilidade += qntd;
 					break;
 				case "Inteligencia":
-					if((Inteligencia -= qntd) > 0)
-					{
-						Inteligencia += qntd;
-						Pontos += qntd;
-					}
+					Inteligencia += qntd;
 					break;
 			}
 		}
-		public void Evoluir()
+	
+	public void subAtributo(string atbt, int qntd)
+	{
+		switch(atbt)
 		{
-			Level += 1;
-			Pontos += 5;
+			case "Força":
+				if((Forca -= qntd) > 0)
+				{
+					Forca -= qntd;
+					Pontos += qntd;
+				}
+				break;
+			case "Agilidade":
+				if((Agilidade -= qntd) > 0)
+				{
+					Agilidade -= qntd;
+					Pontos += qntd;
+				}
+				break;
+			case "Inteligencia":
+				if((Inteligencia -= qntd) > 0)
+				{
+					Inteligencia -= qntd;
+					Pontos += qntd;
+				}
+				break;
 		}
 	}
+	public void Evoluir()
+	{
+		Level += 1;
+		Pontos += 10;
+	}
+}
 }
 
