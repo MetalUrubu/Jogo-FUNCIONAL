5d830d1a64006e7adb9e49d2eda8e3dcf9757a68
victorgomes1234 <50429042+victorgomes1234@users.noreply.github.com>
2019-11-25 21:46:48 -0300

    Adição dos forms

---
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/Criacao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projeto_Andre_2___Jogo_FUNCIONAL
+{
+	public partial class Criacao : Form
+	{
+		public Criacao()
+		{
+			InitializeComponent();
+		}
+		void HumanoRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TrollRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void ElfoRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void GuerreiroRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void MagoRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void ArqueiroRadioCheckedChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void NomeTextFieldTextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void RacaInfoButtonClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ClasseInforButtonClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ConfrimarButtonClick(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
