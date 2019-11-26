969311da4c469c2f3d2477b9b7279c6b4528569e
MetalUrubu <50429060+MetalUrubu@users.noreply.github.com>
2019-11-26 14:14:35 -0300

    Att "AtributosForm"

--- Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/AtributosForm.cs
+++ Projeto Andre 2 - Jogo FUNCIONAL/Projeto Andre 2 - Jogo FUNCIONAL/AtributosForm.cs
@@ -9,34 +9,52 @@ namespace Projeto_Andre_2___Jogo_FUNCIONAL
 		public AtributosForm()
 		{
 			InitializeComponent();
+			pontosNum.Text = Convert.ToString(Program.usuario.status.Pontos);
+			forcaNum.Text = Convert.ToString(Program.usuario.status.Forca);
+			agilidadeNum.Text = Convert.ToString(Program.usuario.status.Agilidade);
+			inteligenciaNum.Text = Convert.ToString(Program.usuario.status.Inteligencia);
+			
 		}
 		void ForcaAddClick(object sender, EventArgs e)
 		{
-	
+			Program.usuario.status.addAtributo("Força",1);
+			attInferface();
 		}
 		void AgilidadeAddClick(object sender, EventArgs e)
 		{
-	
+			Program.usuario.status.addAtributo("Força",1);
+			attInferface();
 		}
 		void InteligenciaAddClick(object sender, EventArgs e)
 		{
-	
+			Program.usuario.status.addAtributo("Agilidade",1);
+			attInferface();
 		}
 		void ForcaSubClick(object sender, EventArgs e)
-		{
-	
+		{	
+			Program.usuario.status.subAtributo("Inteligencia",1);
+			attInferface();
 		}
 		void AgilidadeSubClick(object sender, EventArgs e)
 		{
-	
+			Program.usuario.status.subAtributo("Agilidade",1);
+			attInferface();
 		}
 		void InteligenciaSubClick(object sender, EventArgs e)
 		{
-	
+			Program.usuario.status.subAtributo("Inteligencia",1);
+			attInferface();
 		}
 		void ConfirmarClick(object sender, EventArgs e)
 		{
 	
+		}
+		void attInferface()
+		{
+			pontosNum.Text = Convert.ToString(Program.usuario.status.Pontos);
+			forcaNum.Text = Convert.ToString(Program.usuario.status.Forca);
+			agilidadeNum.Text = Convert.ToString(Program.usuario.status.Agilidade);
+			inteligenciaNum.Text = Convert.ToString(Program.usuario.status.Inteligencia);
 		}
 	}
 }
